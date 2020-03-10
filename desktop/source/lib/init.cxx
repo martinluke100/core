79d046f1fd10a1cb3d8230dd7f4feb449c1d877c
Szymon Kłos <szymon.klos@collabora.com>
2020-03-10 15:51:37 +0100

    jsdialog: signal changed on combobox selection
    Change-Id: Icd9b5559f570ec0a70562f9d5a6fc1887b83ca1f

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -3608,6 +3608,7 @@ static void doc_sendDialogEvent(LibreOfficeKitDocument* /*pThis*/, unsigned nWin
                                 OString posString = OUStringToOString(entryPos, RTL_TEXTENCODING_ASCII_US);
                                 int pos = std::atoi(posString.getStr());
                                 pCombobox->set_active(pos);
+                                pCombobox->signal_changed();
                             }
                         }
                         else if (sAction == "change")
